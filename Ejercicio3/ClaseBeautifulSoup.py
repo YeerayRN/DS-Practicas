8ade67430cc7953a518c31460c7b4ae3b7588349
Yeray <161733740+YeerayRN@users.noreply.github.com>
2026-03-03 19:27:44 +0100

    Correciones y concepto general terminado
    Falta el código de las clases Selenium y BeautifulSoup

--- Ejercicio3/ClaseBeautifulSoup.py
+++ Ejercicio3/ClaseBeautifulSoup.py
@@ -1,3 +1,5 @@
+from ClaseScrape import Scrape
+
 class BeautifulSoup(Scrape):
     def scrape(self):
         return "Scraping con BeautifulSoup" ##Completar según documentación oficial de BeautifulSoup
