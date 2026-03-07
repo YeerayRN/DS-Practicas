266b9ff270167b16ac38cd53e31a57f9f8015cb0
Yeray <161733740+YeerayRN@users.noreply.github.com>
2026-03-07 14:35:49 +0100

    Delete ClaseContexto.py

--- ClaseContexto.py
+++
@@ -1,6 +0,0 @@
-class EstrategiaContexto:
-    def __init__(self, estrategia: Scrape):
-        self.estrategia = estrategia
-
-    def ejecutar_estrategia(self):
-        return self.estrategia.scrape()
